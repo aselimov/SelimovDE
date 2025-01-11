ed65b0aa43215fe72c750439929c3d7058463664
Alex Selimov <alex@alexselimov.com>
2025-01-10 21:13:38 -0500

    Update picom rules for dunst and add album art to mpdnotif.sh

--- bin/mpdnotif.sh
+++ bin/mpdnotif.sh
@@ -3,7 +3,10 @@
 while "true"; do
     status="`mpc status`"
     if [[ ${status} == *"[playing]"* ]]; then
-        dunstify -a Music --replace=27072 -t 2000 -i "$HOME/media/pics/music.png" "Now Playing:" "Artist: $(mpc --format '%artist%' current)\nSong: $(mpc --format '%title%' current)"
+
+        ffmpeg -i ~/media/music/"$(mpc --format "%file%" current)" -an -c:v copy ~/media/music/.now_playing.jpg
+        dunstify -a Music --replace=27072 -t 2000 -i "$HOME/media/music/.now_playing.jpg" "Now Playing:" "Artist: $(mpc --format '%artist%' current)\nSong: $(mpc --format '%title%' current)"
+        rm ~/media/music/.now_playing.jpg
     fi
     mpc current --wait > /dev/null
 done
