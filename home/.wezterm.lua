a724879d948b248f9869dd7b738337e7a5815032
Alex Selimov <alex@alexselimov.com>
2025-01-14 23:43:26 -0500

    Initial wezterm configuration

---
+++ home/.wezterm.lua
@@ -0,0 +1,68 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.enable_tab_bar = false
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font_size = 9
+config.colors = {
+	-- The default text color
+	foreground = "#8e8e8e",
+	-- The default background color
+	background = "#191919",
+
+	-- Overrides the cell background color when the current cell is occupied by the
+	-- cursor and the cursor style is set to Block
+	cursor_bg = "#BBBBBB",
+	-- Overrides the text color when the current cell is occupied by the cursor
+	cursor_fg = "#191919",
+	-- Specifies the border color of the cursor when the cursor style is set to Block,
+	-- or the color of the vertical or horizontal bar when the cursor style is set to
+	-- Bar or Underline.
+	cursor_border = "#BBBBBB",
+
+	-- the foreground color of selected text
+	selection_fg = "#191919",
+	-- the background color of selected text
+	selection_bg = "#BBBBBB",
+
+	-- The color of the scrollbar "thumb"; the portion that represents the current viewport
+	scrollbar_thumb = "#222222",
+
+	-- The color of the split lines between panes
+	split = "#444444",
+
+	ansi = {
+		"#191919",
+		"#DE6E7C",
+		"#819B69",
+		"#B77E64",
+		"#6099C0",
+		"#B279A7",
+		"#66A5AD",
+		"#BBBBBB",
+	},
+	brights = {
+		"#3d3839",
+		"#E8838F",
+		"#8BAE68",
+		"#D68C67",
+		"#61ABDA",
+		"#CF86C1",
+		"#65B8C1",
+		"#8e8e8e",
+	},
+}
+config.window_background_opacity = 0.85
+
+config.keys = {
+	{ key = "j", mods = "CTRL|SHIFT", action = wezterm.action.DecreaseFontSize },
+	{ key = "k", mods = "CTRL|SHIFT", action = wezterm.action.IncreaseFontSize },
+}
+-- and finally, return the configuration to wezterm
+return config -- Pull in the wezterm API
